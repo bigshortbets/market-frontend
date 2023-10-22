025e2dd8ae8a2b503b60d38f66a32953e33b89a7
Kacper Gumieniuk <kacpergumieniuk@gmail.com>
2023-10-22 15:03:36 +0200

    Add Navbar UI component and basic layout

---
+++ components/Navbar/Navbar.tsx
@@ -0,0 +1,14 @@
+import Image from 'next/image';
+import React from 'react';
+import logo from '../../public/logo.svg';
+
+export const Navbar = () => {
+  return (
+    <nav className="bg-primary-bg w-full h-[80px] flex justify-between items-center px-6">
+      <Image src={logo} alt="BigShortBet$ Logo" width={60} />
+      <button className="bg-[#9BA6F8] font-semibold px-4 py-2 text-xl rounded-xl text-[#01083A]">
+        Connect wallet
+      </button>
+    </nav>
+  );
+};
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -13,8 +13,12 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'primary-bg': '#000211',
+        'secondary-bg': '#191B24',
+      },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
