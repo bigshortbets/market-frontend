82952b74a67d837d3f3e8827129c70ba8f273e42
Kacper Gumieniuk <kacpergumieniuk@gmail.com>
2023-10-23 12:48:34 +0200

    Empty state for UI

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -6,7 +6,7 @@ export const Navbar = () => {
   return (
     <nav className="bg-primary-bg w-full h-[80px] flex justify-between items-center px-6">
       <Image src={logo} alt="BigShortBet$ Logo" width={60} />
-      <button className="bg-[#9BA6F8] font-semibold px-4 py-2 text-xl rounded-xl text-[#01083A]">
+      <button className="bg-[#9BA6F8] font-semibold px-4 py-2 text-lg rounded-xl text-[#01083A]">
         Connect wallet
       </button>
     </nav>
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,8 +6,21 @@ const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
-    <main className={`min-h-screen  ${inter.className} bg-secondary-bg `}>
+    <main
+      className={`min-h-screen  ${inter.className} bg-secondary-bg flex flex-col`}
+    >
       <Navbar />
+      {/* Some empty state for UI */}
+      <div className="h-[calc(100vh-80px)] p-6 flex-col flex gap-y-6">
+        <div className="flex-1 flex gap-6 justify-evenly">
+          <div className="animate-pulse bg-primary-bg w-full rounded-xl"></div>
+          <div className="animate-pulse bg-primary-bg w-full rounded-xl"></div>
+        </div>
+        <div className="flex-1 flex gap-6 justify-evenly">
+          <div className="animate-pulse bg-primary-bg w-full rounded-xl"></div>
+          <div className="animate-pulse bg-primary-bg w-full rounded-xl"></div>
+        </div>
+      </div>
     </main>
   );
 }
