aa5ed88a7bfe76b587e44926b9f9edbe678f5c6b
Kacper Gumieniuk <kacpergumieniuk@gmail.com>
2023-10-23 17:57:58 +0200

    Remove unused files, add wagmi and viem, create ConnectButton component

---
+++ components/ConnectButton.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const ConnectButton = () => {
+  return (
+    <button className="bg-[#9BA6F8] font-semibold px-4 py-2 text-lg rounded-xl text-[#01083A]">
+      Connect wallet
+    </button>
+  );
+};
