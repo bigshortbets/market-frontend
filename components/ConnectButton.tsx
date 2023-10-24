ca39fec59baceb68c875abf5be4e597ed97c5abf
Kacper Gumieniuk <kacpergumieniuk@gmail.com>
2023-10-24 14:03:16 +0200

    Handle discconection

--- components/ConnectButton.tsx
+++ components/ConnectButton.tsx
@@ -1,16 +1,21 @@
 import { useEffect, useState } from 'react';
-import { useAccount, useConnect } from 'wagmi';
+import { useAccount, useConnect, useDisconnect } from 'wagmi';
 
 export const ConnectButton = () => {
   const { connect, connectors } = useConnect();
+  const { disconnect } = useDisconnect();
   const { isConnected } = useAccount();
 
   const connector = connectors[0];
 
+  const handleConnection = () => {
+    isConnected ? disconnect() : connect({ connector });
+  };
+
   return (
     <button
       className="bg-[#9BA6F8] font-semibold px-4 py-2 text-lg rounded-xl text-[#01083A]"
-      onClick={() => connect({ connector })}
+      onClick={handleConnection}
     >
       {isConnected ? 'Connected' : 'Connect wallet'}
     </button>
