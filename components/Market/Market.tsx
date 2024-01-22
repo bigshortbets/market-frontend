4e59e60c2e3f39e559955d6e828d5b4c783ef64f
Kacper Gumieniuk <kacpergumieniuk@gmail.com>
2024-01-22 09:23:52 +0100

    Responsivity fixes

--- components/Market/Market.tsx
+++ components/Market/Market.tsx
@@ -1,24 +1,24 @@
-import { useEffect } from "react";
-import { MarketBar } from "../MarketBar";
-import { Navbar } from "../Navbar/Navbar";
-import { MarketType } from "@/types/marketTypes";
-import { atom, useAtom } from "jotai";
-import { ContractDetails } from "./ContractDetails/ContractDetails";
-import { OrderManager } from "./OrderManager/OrderManager";
-import { TradingHub } from "./TradingHub/TradingHub";
-import { useNetwork, useSwitchNetwork } from "wagmi";
-import { Mobile } from "../Mobile";
-import { OrderBookContainer } from "./OrderBook/OrderBookContainer";
-import { FinanceManager } from "./FinanceManager/FinanceManager";
+import { useEffect } from 'react';
+import { MarketBar } from '../MarketBar';
+import { Navbar } from '../Navbar/Navbar';
+import { MarketType } from '@/types/marketTypes';
+import { atom, useAtom } from 'jotai';
+import { ContractDetails } from './ContractDetails/ContractDetails';
+import { OrderManager } from './OrderManager/OrderManager';
+import { TradingHub } from './TradingHub/TradingHub';
+import { useNetwork, useSwitchNetwork } from 'wagmi';
+import { Mobile } from '../Mobile';
+import { OrderBookContainer } from './OrderBook/OrderBookContainer';
+import { FinanceManager } from './FinanceManager/FinanceManager';
 
 interface MarketProps {
   markets: MarketType[];
 }
 
-export type UIConfigurationType = "HubOrder" | "OrderHub";
+export type UIConfigurationType = 'HubOrder' | 'OrderHub';
 
-export const selectedMarketIdAtom = atom<string>("");
-export const UIConfigurationAtom = atom<UIConfigurationType>("HubOrder");
+export const selectedMarketIdAtom = atom<string>('');
+export const UIConfigurationAtom = atom<UIConfigurationType>('HubOrder');
 
 export const Market = ({ markets }: MarketProps) => {
   const [, setSelectedMarketId] = useAtom(selectedMarketIdAtom);
@@ -35,14 +35,14 @@ export const Market = ({ markets }: MarketProps) => {
   const [UIConfiguration] = useAtom(UIConfigurationAtom);
 
   return (
-    <div className="h-screen w-full">
+    <div className="min-h-screen w-full">
       <div className={`h-full bg-primary-bg flex-col hidden lg:flex`}>
         <Navbar />
         <MarketBar markets={markets} />
-        <div className="h-[(100vh-120px)] px-6 py-6">
+        <div className="px-6 py-6" style={{ height: 'calc(100vh - 120px)' }}>
           <div
             className={`flex justify-between gap-6 h-full ${
-              UIConfiguration === "HubOrder" ? "flex-row" : "flex-row-reverse"
+              UIConfiguration === 'HubOrder' ? 'flex-row' : 'flex-row-reverse'
             }`}
           >
             <TradingHub />
@@ -50,7 +50,6 @@ export const Market = ({ markets }: MarketProps) => {
               <OrderBookContainer />
             </div>
             <div className="flex flex-col gap-6 h-full">
-              {/*  <OrderManager markets={markets} /> */}
               <FinanceManager markets={markets} />
               <ContractDetails markets={markets} />
             </div>
