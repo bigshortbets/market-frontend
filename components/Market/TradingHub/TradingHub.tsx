e7e230dbe88b62664e89ff1139f10b4ce55e3069
kacpergumieniuk <kacpergumieniuk@gmail.com>
2024-02-14 08:59:29 +0100

    Working trading hub, temporary style

--- components/Market/TradingHub/TradingHub.tsx
+++ components/Market/TradingHub/TradingHub.tsx
@@ -14,20 +14,13 @@ export const TradingHub = () => {
 
   return (
     <div className="flex-grow h-full border-[#444650] border rounded-[10px] flex flex-col bg-[#191B24]">
-      {/* <div className="w-full bg-primary-bg h-[45px]">
-        <div className="bg-secondary-bg rounded-t w-1/2 h-full flex p-1 gap-1">
-          {tabs.map((tab, key) => (
-            <TradingHubTab key={key} value={tab} />
-          ))}
-        </div>
-      </div>
-      {address && <TradingHubContentContainer />} */}
       <div className="flex-grow">
         <div className="px-2.5 pt-3 pb-2.5 flex gap-1">
           {tabs.map((tab, key) => (
             <TradingHubTab key={key} value={tab} />
           ))}
         </div>
+        {address && <TradingHubContentContainer />}
       </div>
       <div className="h-[58px] border-t border-[#444650] "></div>
     </div>
