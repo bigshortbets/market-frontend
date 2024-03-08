aa6a65535b7dbb3d2c0f8ee55b4762a0ed89cbc5
Kacper Gumieniuk <kacpergumieniuk@gmail.com>
2024-03-08 11:04:01 +0100

    Precise support of tickers for TradingView charts

--- utils/getMarketDetails.ts
+++ utils/getMarketDetails.ts
@@ -1,36 +1,38 @@
-import { MarketsDataProps, marketsData } from "@/data/marketsData";
+import { MarketsDataProps, marketsData } from '@/data/marketsData';
 
 const monthMapping: { [key: string]: string } = {
-  F: "January",
-  G: "February",
-  H: "March",
-  J: "April",
-  K: "May",
-  M: "June",
-  N: "July",
-  Q: "August",
-  U: "September",
-  V: "October",
-  X: "November",
-  Z: "December",
+  F: 'January',
+  G: 'February',
+  H: 'March',
+  J: 'April',
+  K: 'May',
+  M: 'June',
+  N: 'July',
+  Q: 'August',
+  U: 'September',
+  V: 'October',
+  X: 'November',
+  Z: 'December',
 };
 
 export const getMarkeDetails = (
   marketInput: string
 ): MarketsDataProps | undefined => {
+  const inputParts = marketInput.split(':');
+  const actualInput = inputParts.length > 1 ? inputParts[1] : inputParts[0];
   const market = marketsData.find((market) =>
-    marketInput.startsWith(market.symbol)
+    actualInput.startsWith(market.symbol)
   );
 
   if (market) {
     const symbolLength = market.symbol.length;
-    const symbol = marketInput.substr(0, symbolLength);
-    let monthCode = "";
-    let yearCode = "";
-    for (let i = marketInput.length - 1; i >= 0; i--) {
-      if (isNaN(Number(marketInput[i]))) {
-        monthCode = marketInput[i];
-        yearCode = marketInput.substring(i + 1);
+    const symbol = actualInput.substr(0, symbolLength);
+    let monthCode = '';
+    let yearCode = '';
+    for (let i = actualInput.length - 1; i >= 0; i--) {
+      if (isNaN(Number(actualInput[i]))) {
+        monthCode = actualInput[i];
+        yearCode = actualInput.substring(i + 1);
         break;
       }
     }
