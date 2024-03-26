56abadcbf26f405f8dd49ed2eaab1686ed026708
Kacper Gumieniuk <kacpergumieniuk@gmail.com>
2024-03-26 10:34:42 +0100

    Improve order manager experience

--- components/Market/OrderManager/OrderManager.tsx
+++ components/Market/OrderManager/OrderManager.tsx
@@ -13,6 +13,9 @@ import { useWeb3Modal } from '@web3modal/wagmi/react';
 import { bigshortbetsChain } from '@/blockchain/chain';
 import { switchToBigShortBetsChain } from '@/utils/switchToBigShortBetsChain';
 import { calculateMarketClosing } from '@/utils/calculateMarketClosing';
+import { currencyFormatter } from '@/utils/currencyFormatter';
+import { IoMdInformationCircle } from 'react-icons/io';
+import { Tooltip } from 'react-tooltip';
 
 export enum OrderSideEnum {
   LONG,
@@ -179,12 +182,40 @@ export const OrderManager = ({ markets }: OrderManagerProps) => {
         </p>
         <div className="flex flex-col gap-2 ">
           <div className="flex justify-between items-center font-semibold text-[13px] text-secondary ">
-            <p>Cost of the order</p>
-            <p>{!noMarkets ? orderCost.toFixed(2) : '-'} USDC</p>
+            <div className="flex items-center gap-1">
+              <p>Order cost</p>
+              <a
+                data-tooltip-id="order-cost-tooltip"
+                data-tooltip-html={`<b>Order cost</b> = Initial margin * ( Order price * Quantity * Contract unit )</br> <b>${currencyFormatter.format(
+                  orderCost
+                )}</b> = ${
+                  Number(selectedMarket?.initialMargin) / 100
+                } * ( ${price} * ${quantity} * ${Number(
+                  selectedMarket?.contractUnit
+                )} )`}
+              >
+                <IoMdInformationCircle className="text-[#7F828F] text-sm " />
+              </a>
+            </div>
+            <p>{!noMarkets ? currencyFormatter.format(orderCost) : '-'} USDC</p>
           </div>
           <div className="flex justify-between items-center font-semibold text-xs text-tetriary ">
-            <p>Order value</p>
-            <p>{!noMarkets ? orderValue.toFixed(2) : '-'} USDC</p>
+            <div className="flex items-center gap-1">
+              <p>Order value</p>
+              <a
+                data-tooltip-id="order-value-tooltip"
+                data-tooltip-html={`<b>Order value</b> = Order Price * Quantity * Contract Unit</br> <b>${currencyFormatter.format(
+                  orderValue
+                )}</b> = ${price} * ${quantity} * ${Number(
+                  selectedMarket?.contractUnit
+                )}`}
+              >
+                <IoMdInformationCircle className="text-[#7F828F] text-sm " />
+              </a>
+            </div>
+            <p>
+              {!noMarkets ? currencyFormatter.format(orderValue) : '-'} USDC
+            </p>
           </div>
         </div>
 
@@ -218,101 +249,8 @@ export const OrderManager = ({ markets }: OrderManagerProps) => {
       {noMarkets && (
         <FinanceManagerWarning error={`Currently no markets are available.`} />
       )}
+      <Tooltip id="order-cost-tooltip" style={{ fontSize: '12px' }} />
+      <Tooltip id="order-value-tooltip" style={{ fontSize: '12px' }} />
     </div>
-    /* <div
-      className={`w-full h-[400px] rounded flex flex-col  transition ease-in-out px-1`}
-    >
-      <div className="mb-3">
-        <div className="flex flex-col mb-3">
-          <label
-            htmlFor="orderPriceInput"
-            className="leading-4 text-xs text-[#7F828F] font-semibold mb-1"
-          >
-            Order price
-          </label>
-          <div className="relative">
-            <NumericFormat
-              allowNegative={false}
-              id={"orderPriceInput"}
-              className="outline-none bg-[#23252E] border-none text-sm text-white py-3 rounded-lg px-3 w-full"
-              onChange={(e) => setPrice(Number(e.target.value))}
-              value={price}
-            />
-            <span className="absolute right-3 bottom-[14px] text-xs opacity-50">
-              USDC
-            </span>
-          </div>
-        </div>
-        <div className="flex flex-col mb-5">
-          <label
-            htmlFor="quantityInput"
-            className="leading-4 text-xs text-[#7F828F] font-semibold mb-1"
-          >
-            Quantity
-          </label>
-          <NumericFormat
-            allowNegative={false}
-            id={"quantityInput"}
-            className="outline-none bg-[#23252E] border-none text-sm text-white py-3 rounded-lg px-3"
-            value={quantity}
-            onChange={(e) => setQuantity(Number(e.target.value))}
-          />
-        </div>
-        <div className="flex justify-between mb-2">
-          <div className="flex items-center gap-1">
-            <p className="text-xs">Order cost</p>
-            <a
-              data-tooltip-id="order-cost-tooltip"
-              data-tooltip-html={`<b>Order cost</b> = Initial margin * ( Order price * Quantity * Contract unit )</br> <b>${orderCost}</b> = ${
-                Number(selectedMarket?.initialMargin) / 100
-              } * ( ${price} * ${quantity} * ${Number(
-                selectedMarket?.contractUnit
-              )} )`}
-            >
-              <IoMdInformationCircle className="text-[#7F828F] text-sm " />
-            </a>
-          </div>
-
-          <p className="text-xs">{orderCost.toFixed(2)} USDC</p>
-        </div>
-        <div className="flex justify-between mb-3">
-          <div className="flex items-center gap-1">
-            <p className="text-xs">Order value</p>
-            <a
-              data-tooltip-id="order-value-tooltip"
-              data-tooltip-html={`<b>Order value</b> = Order Price * Quantity * Contract Unit</br> <b>${orderValue}</b> = ${price} * ${quantity} * ${Number(
-                selectedMarket?.contractUnit
-              )}`}
-            >
-              <IoMdInformationCircle className="text-[#7F828F] text-sm " />
-            </a>
-          </div>
-
-          <p className="text-xs">{orderValue.toFixed(2)} USDC</p>
-        </div>
-      </div>
-      <div className="flex items-center gap-2 ">
-        <button
-          className={`flex-1  duration-300 transition bg-[#73D391] hover:bg-[#61C27B] ease-in-out py-2 flex flex-col items-center rounded-lg ${
-            isActionDisabled && "opacity-50 pointer-events-none"
-          }`}
-          onClick={() => writeLongOrder?.()}
-        >
-          <p className="text-white text-sm font-bold">BUY</p>
-          <p className="text-white text-xs font-bold">LONG</p>
-        </button>
-        <button
-          className={`flex-1  duration-300 bg-[#D26D6C] hover:bg-[#C53F3A] transition ease-in-out py-2 flex flex-col items-center rounded-lg ${
-            isActionDisabled && "opacity-50 pointer-events-none"
-          }`}
-          onClick={() => writeShortOrder?.()}
-        >
-          <p className="text-white text-sm font-bold rounded-lg">SELL</p>
-          <p className="text-wwhite text-xs font-bold">SHORT</p>
-        </button>
-      </div>
-      <Tooltip id="order-cost-tooltip" style={{ fontSize: "12px" }} />
-      <Tooltip id="order-value-tooltip" style={{ fontSize: "12px" }} />
-    </div> */
   );
 };
