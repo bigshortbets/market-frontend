0e4afdff922b358eced9f5f94b0f19b603c48e9b
Kacper Gumieniuk <kacpergumieniuk@gmail.com>
2024-07-04 11:48:22 +0200

    Charts prototype

--- components/Market/TradingHub/TradingHub.tsx
+++ components/Market/TradingHub/TradingHub.tsx
@@ -6,6 +6,11 @@ import { AggregatedPositionsCheckbox } from './TradingHubPositions/AggregatedPos
 import { useState } from 'react';
 import { tradingHubStateAtom } from '@/store/store';
 import { TradingHubFooter } from './TradingHubFooter';
+import {
+  Chart,
+  LineSeries,
+  CandlestickSeries,
+} from 'lightweight-charts-react-wrapper';
 
 const tabs = ['positions', 'orders', 'history' /* , 'chat' */] as const;
 export type TradingHubStateType = (typeof tabs)[number];
@@ -18,6 +23,23 @@ export const TradingHub = () => {
   const toggleIsAggregated = () => {
     isAggregated ? setIsAggregated(false) : setIsAggregated(true);
   };
+
+  const [data, setData] = useState([
+    { time: '2019-04-11', value: 80.01 },
+    { time: '2019-04-12', value: 96.63 },
+    { time: '2019-04-13', value: 76.64 },
+    { time: '2019-04-14', value: 81.89 },
+    { time: '2019-04-15', value: 74.43 },
+    { time: '2019-04-16', value: 80.01 },
+    { time: '2019-04-17', value: 96.63 },
+    { time: '2019-04-18', value: 76.64 },
+    { time: '2019-04-19', value: 81.89 },
+    { time: '2019-04-20', value: 74.43 },
+    { time: '2019-04-22', value: 80.01 },
+    { time: '2019-04-25', value: 100.01 },
+    { time: '2019-04-26', value: 105.01 },
+    { time: '2019-04-27', value: 1.01 },
+  ]);
 
   return (
     <div className=' h-[calc(100vh-166px)] lg:flex-1 lg:h-full border-[#444650] border rounded-[10px] flex flex-col bg-[#191B24]'>
@@ -39,6 +61,9 @@ export const TradingHub = () => {
         </div>
         {address && <TradingHubContentContainer isAggregated={isAggregated} />}
       </div>
+      <Chart width={400} height={200}>
+        <LineSeries data={data} />
+      </Chart>
       <TradingHubFooter />
     </div>
   );
