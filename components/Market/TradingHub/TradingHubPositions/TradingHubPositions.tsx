cde1effa21c73e64fae22a912460c6d3d488b3aa
kacpergumieniuk <kacpergumieniuk@gmail.com>
2024-07-23 09:26:58 +0200

    Add sorting logic

--- components/Market/TradingHub/TradingHubPositions/TradingHubPositions.tsx
+++ components/Market/TradingHub/TradingHubPositions/TradingHubPositions.tsx
@@ -5,6 +5,7 @@ import { useAtom } from 'jotai';
 import { unsettledLossesAtom } from '../../Market';
 import { useAccount } from 'wagmi';
 import { TradingHubNonAggregatedPositions } from './TradingHubNonAggregatedPositions';
+import { getMarkeDetails } from '@/utils/getMarketDetails';
 
 interface TradingHubPositionsProps {
   positions: PositionType[];
@@ -30,16 +31,41 @@ export const TradingHubPositions = ({
   const positionsByMarketTicker = aggregatePositionsByMarketTicker();
   const { address } = useAccount();
 
+  /* Sorting alphabetically  */
+
+  const sortPositionsByMarketName = (
+    positionsByMarketTicker: Record<string, PositionType[]>
+  ) => {
+    const sortedTickers = Object.keys(positionsByMarketTicker).sort((a, b) => {
+      const nameA = getMarkeDetails(a)?.name || a;
+      const nameB = getMarkeDetails(b)?.name || b;
+      return nameA.localeCompare(nameB);
+    });
+
+    const sortedPositionsByMarketTicker: Record<string, PositionType[]> = {};
+    sortedTickers.forEach((ticker) => {
+      sortedPositionsByMarketTicker[ticker] = positionsByMarketTicker[ticker];
+    });
+
+    return sortedPositionsByMarketTicker;
+  };
+
+  /*  */
+
+  const sortedPositionsByMarketTicker = sortPositionsByMarketName(
+    positionsByMarketTicker
+  );
   return (
     <div
       className='w-full h-full  px-2.5  overflow-y-auto no-scrollbar'
       style={{ maxHeight: 'calc(100vh - 230px)' }}
+      onClick={() => console.log(positionsByMarketTicker)}
     >
       {positions.length > 0 ? (
         <>
           {isAggregated ? (
             <div className='flex flex-col gap-4'>
-              {Object.entries(positionsByMarketTicker).map(
+              {Object.entries(sortedPositionsByMarketTicker).map(
                 ([ticker, positions]) => (
                   <TradingHubAggregatedPosition
                     key={ticker}
