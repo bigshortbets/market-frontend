d8a168b3107afaa4bbd5fa7128097e9f517946d6
Kacper Gumieniuk <48014664+kacpergumieniuk@users.noreply.github.com>
2024-07-24 11:13:24 +0200

    #103 change order section (#128)
    * New order manager style
    
    * Fix input values in order manager

--- components/Market/OrderManager/OrderManager.tsx
+++ components/Market/OrderManager/OrderManager.tsx
@@ -14,10 +14,9 @@ import { bigshortbetsChain } from '@/blockchain/chain';
 import { switchToBigShortBetsChain } from '@/utils/switchToBigShortBetsChain';
 import { calculateMarketClosing } from '@/utils/calculateMarketClosing';
 import { currencyFormatter } from '@/utils/currencyFormatter';
-import { IoMdInformationCircle } from 'react-icons/io';
 import { Tooltip } from 'react-tooltip';
 import { currencySymbol } from '@/blockchain/constants';
-import { FinanceManagerInfo } from '../FinanceManager/FinanceManagerInfo';
+import ReactLoading from 'react-loading';
 
 export enum OrderSideEnum {
   LONG,
@@ -29,17 +28,17 @@ interface OrderManagerProps {
 }
 
 export const OrderManager = ({ markets }: OrderManagerProps) => {
-  const [price, setPrice] = useState<number>(1);
-  const [quantity, setQuantity] = useState<number>(1);
-  const [selectedSideOrder, setSelectedSideOrder] = useState<OrderSideEnum>(
-    OrderSideEnum.LONG
-  );
+  const [price, setPrice] = useState<string>('1');
+  const [quantity, setQuantity] = useState<string>('1');
 
   const [currentBlock] = useAtom(currentBlockAtom);
   const [selectedMarketId] = useAtom(selectedMarketIdAtom);
   const selectedMarket = findMarketById(markets, selectedMarketId);
   const [isDivisibleByTickSize, setIsDivisibleByTickSize] = useState(
-    checkIfDivisible(price, Number(selectedMarket?.tickSize.toString()!))
+    checkIfDivisible(
+      Number(price),
+      Number(selectedMarket?.tickSize.toString()!)
+    )
   );
 
   const { isClosed: isMarketClosed } = calculateMarketClosing(
@@ -48,56 +47,50 @@ export const OrderManager = ({ markets }: OrderManagerProps) => {
   );
 
   const { address, chain } = useAccount();
-
   const { formattedBalance } = useNativeCurrencyBalance(address);
 
-  const { write: writeShortOrder } = useCreateOrderWrite(
-    price,
-    quantity,
-    OrderSideEnum.SHORT
-  );
-  const { write: writeLongOrder } = useCreateOrderWrite(
-    price,
-    quantity,
-    OrderSideEnum.LONG
-  );
+  const { write: writeShortOrder, isLoading: isShortLoading } =
+    useCreateOrderWrite(Number(price), Number(quantity), OrderSideEnum.SHORT);
+  const { write: writeLongOrder, isLoading: isLongLoading } =
+    useCreateOrderWrite(Number(price), Number(quantity), OrderSideEnum.LONG);
 
   const orderCost = Math.max(
     500,
     (Number(selectedMarket?.initialMargin) / 100) *
       (Number(selectedMarket?.oraclePrice) *
-        quantity *
+        Number(quantity) *
         Number(selectedMarket?.contractUnit))
   );
 
-  const orderValue = price * quantity * Number(selectedMarket?.contractUnit);
+  const orderValue =
+    Number(price) * Number(quantity) * Number(selectedMarket?.contractUnit);
 
   const isBsbNetwork = chain?.id === bigshortbetsChain.id;
 
   const isActionDisabled =
     !address ||
     isMarketClosed ||
-    price === 0 ||
+    Number(price) === 0 ||
     orderCost + 50 > Number(formattedBalance) ||
     !isDivisibleByTickSize ||
-    quantity === 0 ||
+    Number(quantity) === 0 ||
     selectedMarket?.oraclePrice === null;
 
   useEffect(() => {
     selectedMarket?.oraclePrice &&
-      setPrice(Number(selectedMarket?.oraclePrice.toString()));
+      setPrice(selectedMarket?.oraclePrice.toString());
   }, [selectedMarketId]);
 
   useEffect(() => {
     const res = checkIfDivisible(
-      price,
+      Number(price),
       Number(selectedMarket?.tickSize.toString()!)
     );
 
     setIsDivisibleByTickSize(res);
   }, [price]);
 
-  const handleWriteOrder = () => {
+  const handleWriteOrder = (side: OrderSideEnum) => {
     if (!address) {
       open();
       return;
@@ -106,10 +99,10 @@ export const OrderManager = ({ markets }: OrderManagerProps) => {
       switchToBigShortBetsChain();
       return;
     }
-    if (selectedSideOrder === OrderSideEnum.LONG) {
+    if (side === OrderSideEnum.LONG) {
       writeLongOrder();
     }
-    if (selectedSideOrder === OrderSideEnum.SHORT) {
+    if (side === OrderSideEnum.SHORT) {
       writeShortOrder();
     }
   };
@@ -117,11 +110,9 @@ export const OrderManager = ({ markets }: OrderManagerProps) => {
   const noMarkets = markets.length < 1;
 
   return (
-    <div className='p-2.5 pb-4 flex flex-col gap-4'>
-      <div className='flex flex-col gap-2'>
-        <p className='text-sm font-semibold text-secondary leading-[24px]'>
-          Order
-        </p>
+    <div className='px-2.5 pt-3 pb-4 flex flex-col gap-4'>
+      <div className='flex flex-col gap-2.5'>
+        {/* Price input */}
         <div className='flex flex-col'>
           <label
             htmlFor='orderPriceInput'
@@ -134,7 +125,7 @@ export const OrderManager = ({ markets }: OrderManagerProps) => {
               allowNegative={false}
               id={'orderPriceInput'}
               className='text-right outline-none  w-[85%] bg-[#23252E] '
-              onChange={(e) => setPrice(Number(e.target.value))}
+              onChange={(e) => setPrice(e.target.value)}
               value={price}
             />
 
@@ -143,6 +134,8 @@ export const OrderManager = ({ markets }: OrderManagerProps) => {
             </span>
           </div>
         </div>
+        {/*  */}
+        {/* Quantity input */}
         <div className='flex flex-col'>
           <label
             htmlFor='quantityInput'
@@ -155,7 +148,7 @@ export const OrderManager = ({ markets }: OrderManagerProps) => {
               allowNegative={false}
               id={'quantityInput'}
               className='text-right outline-none  w-[85%] bg-[#23252E]'
-              onChange={(e) => setQuantity(Number(e.target.value))}
+              onChange={(e) => setQuantity(e.target.value)}
               decimalScale={0}
               value={quantity}
             />
@@ -164,65 +157,32 @@ export const OrderManager = ({ markets }: OrderManagerProps) => {
             </span>
           </div>
         </div>
-      </div>
-
-      <div className='flex items-center gap-2 '>
-        <div
-          className={`${
-            selectedSideOrder === OrderSideEnum.LONG
-              ? 'text-[#87DAA4] border border-[#87DAA4]'
-              : 'text-tetriary border border-[#23252E]'
-          } flex-1   bg-[#23252E] py-2 flex flex-col items-center rounded-lg cursor-pointer `}
-          onClick={() => setSelectedSideOrder(OrderSideEnum.LONG)}
-        >
-          <p className=' text-[13px] font-semibold'>Buy</p>
-          <p className=' text-[10px]'>Long</p>
-        </div>
-        <div
-          className={`${
-            selectedSideOrder === OrderSideEnum.SHORT
-              ? 'text-[#E4ADAC] border border-[#E4ADAC]'
-              : 'text-tetriary border border-[#23252E]'
-          } flex-1   bg-[#23252E] py-2 flex flex-col items-center rounded-lg cursor-pointer `}
-          onClick={() => setSelectedSideOrder(OrderSideEnum.SHORT)}
-        >
-          <p className=' text-[13px] font-semibold'>Sell</p>
-          <p className='text-[10px]'>Short</p>
-        </div>
-      </div>
-      <div className='p-2 rounded-lg bg-[#000211] flex flex-col gap-4'>
-        <p className='text-sm font-semibold text-secondary leading-[24px]'>
-          Summary
-        </p>
-        <div className='flex flex-col gap-2 '>
-          <div className='flex justify-between items-center font-semibold text-[13px] text-secondary '>
-            <div className='flex items-center gap-1'>
-              <p>Order Cost</p>
-              <a
-                data-tooltip-id='order-cost-tooltip'
-                data-tooltip-html={`Mandatory initial deposit, set at ${Number(
-                  selectedMarket?.initialMargin
-                )}%</br> of the contract value being traded,</br> but not lower than 500 ${currencySymbol}.`}
-              >
-                <IoMdInformationCircle className='text-[#7F828F] text-sm ' />
-              </a>
-            </div>
-            <p>
+        {/*  */}
+        <div className='flex flex-col gap-1 px-1 mt-1'>
+          <div className='flex items-center justify-between gap-2 mt-1'>
+            <a
+              className='text-[13px] decoration-dotted	underline cursor-help	'
+              data-tooltip-id='order-cost-tooltip'
+              data-tooltip-html={`Order Cost is mandatory initial deposit,</br> set at ${Number(
+                selectedMarket?.initialMargin
+              )}% of the contract value being traded,</br> but not lower than 500 ${currencySymbol}.`}
+            >
+              Order Cost
+            </a>
+            <p className='text-[13px] font-semibold'>
               {!noMarkets ? currencyFormatter.format(orderCost) : '-'}{' '}
               {currencySymbol}
             </p>
           </div>
-          <div className='flex justify-between items-center font-semibold text-xs text-tetriary '>
-            <div className='flex items-center gap-1'>
-              <p>Order Value</p>
-              <a
-                data-tooltip-id='order-value-tooltip'
-                data-tooltip-html={`Represents the total value of the underlying asset.</br> It considers the current price of the asset,</br> the quantity of contracts traded, and the</br> standardized units per contract.`}
-              >
-                <IoMdInformationCircle className='text-[#7F828F] text-sm ' />
-              </a>
-            </div>
-            <p>
+          <div className='flex items-center gap-2 mt-1 justify-between'>
+            <a
+              className='text-[12px] text-tetriary decoration-dotted	underline cursor-help	'
+              data-tooltip-id='order-value-tooltip'
+              data-tooltip-html={`Order Value represents the total value of the</br> underlying asset. It considers the current price of</br>  the asset,the quantity of contracts traded, and the</br> standardized units per contract.`}
+            >
+              Order Value
+            </a>
+            <p className='text-[12px] text-tetriary font-semibold'>
               {!noMarkets && selectedMarket?.oraclePrice != null
                 ? currencyFormatter.format(orderValue)
                 : '-'}{' '}
@@ -230,26 +190,49 @@ export const OrderManager = ({ markets }: OrderManagerProps) => {
             </p>
           </div>
         </div>
+      </div>
 
+      <div className='flex items-center gap-2 '>
         <button
-          onClick={handleWriteOrder}
+          onClick={() => handleWriteOrder(OrderSideEnum.LONG)}
           disabled={isActionDisabled}
-          className={`disabled:bg-gray-400 w-full rounded-lg ${
-            address && isBsbNetwork
-              ? selectedSideOrder === OrderSideEnum.LONG
-                ? 'bg-[#87DAA4]'
-                : 'bg-[#D26D6C]'
-              : 'bg-[#4ECB7D]'
-          } text-[#01083A] text-[13px] font-semibold py-3`}
+          className={`transition justify-center ease-in flex-1 text-white flex flex-col items-center rounded-lg h-[50px] ${
+            isActionDisabled
+              ? 'bg-gray-400 cursor-not-allowed'
+              : 'bg-[#569b6e] hover:bg-[#4c8660]'
+          }`}
         >
-          {!address && 'Place Order'}
-          {address && isBsbNetwork && 'Place Order'}
-          {address && !isBsbNetwork && 'Change Network'}
+          {isLongLoading ? (
+            <ReactLoading type='spin' height={22} width={22} color='white' />
+          ) : (
+            <div>
+              <p className=' text-[13px] font-semibold'>Buy</p>
+              <p className=' text-[10px]'>Long</p>
+            </div>
+          )}
+        </button>
+        <button
+          onClick={() => handleWriteOrder(OrderSideEnum.SHORT)}
+          disabled={isActionDisabled}
+          className={`transition justify-center ease-in flex-1 text-white flex flex-col items-center rounded-lg h-[50px] ${
+            isActionDisabled
+              ? 'bg-gray-400 cursor-not-allowed'
+              : 'bg-[#8f605f] hover:bg-[#7a504f]'
+          }`}
+        >
+          {isShortLoading ? (
+            <ReactLoading type='spin' height={22} width={22} color='white' />
+          ) : (
+            <div>
+              <p className=' text-[13px] font-semibold'>Sell</p>
+              <p className='text-[10px]'>Short</p>
+            </div>
+          )}
         </button>
       </div>
-      {/* <FinanceManagerInfo value='Placing orders take your funds ' /> */}
+
       {!address && (
-        <FinanceManagerWarning error='Connect your wallet to interact with the market.' />
+        <FinanceManagerWarning error='Connect your wallet to place your order.' />
       )}
       {address && Number(formattedBalance) === 0 && (
         <FinanceManagerWarning
@@ -277,7 +260,7 @@ export const OrderManager = ({ markets }: OrderManagerProps) => {
       {address && isMarketClosed && (
         <FinanceManagerWarning error='This market is already closed.' />
       )}
-      {address && quantity === 0 && (
+      {address && Number(quantity) === 0 && (
         <FinanceManagerWarning error='Your quantity value must be higher than 0.' />
       )}
       {!isDivisibleByTickSize &&
